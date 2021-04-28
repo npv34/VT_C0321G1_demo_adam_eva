3acd16247166f19b3a5ed0bf8b655372d4f82a3e
npv34 <phanvanluan611996@gmail.com>
2021-04-28 09:51:33 +0700

    demo adam eva

---
+++ js/Apple.js
@@ -0,0 +1,17 @@
+class Apple {
+    constructor() {
+        this.weight = 10;
+    }
+
+    isEmpty() {
+        return this.weight === 0;
+    }
+
+    getWeight() {
+        return this.weight;
+    }
+
+    decrease() {
+        this.weight -= 1;
+    }
+}
